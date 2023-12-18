efcb672141ab60f184fd8f372f0c7c99dd7e1698
Magnuzzs <magnus_richter@gmx.de>
2023-12-18 18:48:32 +0100

    first Commit hopefully

--- src/Board.hs
+++ src/Board.hs
@@ -4,6 +4,9 @@ module Board where  -- do NOT CHANGE export of module
 -- Note: Imports allowed that DO NOT REQUIRE TO ANY CHANGES TO package.yaml, e.g.:
 --       import Data.Chars
 
+--import Text.Parsec
+--import Text.Parsec.Char
+
 
 -- #############################################################################
 -- ############# GIVEN IMPLEMENTATION                           ################
@@ -38,7 +41,13 @@ instance Eq Cell where
 -- #############################################################################
 
 validateFEN :: String -> Bool
-validateFEN _ = True
+validateFEN fenstring = validateHelper1 fenstring && validateHelper2 fenstring
+
+validateHelper1 :: String -> Bool
+validateHelper1 fenstring = (filter (\c -> c == ',' || c == '/') fenstring) == ",,,,,/,,,,,/,,,,,/,,,,,/,,,,,/,,,,,"
+
+validateHelper2 :: String -> Bool
+validateHelper2 fenstring = (filter (\c -> c == 'r' || c == 'b' || c == ',' || c == '/') fenstring) == fenstring
 
 
 -- #############################################################################
@@ -47,9 +56,28 @@ validateFEN _ = True
 -- ####################### - 1 Coverage Point            #######################
 -- #############################################################################
 
-buildBoard :: String -> Board
-buildBoard _ = []
+{-
+_playerParser :: Char -> Player
+_playerParser 'r' = Red
+_playerParser 'b' = Blue
 
+charToPlayer :: Parsec String () Player
+charToPlayer = _playerParser <$> oneOf "rb"
+
+_playerToCell :: [Player] -> Cell
+_playerToCell [] = Stack[]
+_playerToCell cs = Stack cs
+
+cell :: Parsec String () Cell
+cell = _playerToCell <$> many charToPlayer
+
+board :: Parsec String () Board
+board = sepBy (sepBy cell (char ',')) (char '/')
+
+
+buildBoard :: String -> Either ParseError Board
+buildBoard = parse board ""
+-}
 
 -- #############################################################################
 -- #################### path :: Pos -> Dir -> Int -> [Pos]  ####################
@@ -57,5 +85,43 @@ buildBoard _ = []
 -- #################### - 1 Coverage Point                  ####################
 -- #############################################################################
 
+moveCol :: Char -> Int -> Char
+moveCol c offset = toEnum $ fromEnum c + offset
+
+moveRow :: Int -> Int -> Int
+moveRow r offset = r + offset
+
+movePos :: Pos -> Int -> Int -> Pos
+movePos (Pos col row) colOffset rowOffset =
+  Pos (moveCol col colOffset) (moveRow row rowOffset)
+
 path :: Pos -> Dir -> Int -> [Pos]
-path _ _ _ = []
+path pos _ 0 = [pos]  -- Base case: stop when the integer argument is 0
+path pos North int
+  | row pos == 6 = path pos South (int)
+  | otherwise = pos : path (movePos pos 0 1) North (int-1)
+path pos East int
+  | col pos == 'f' = path pos West (int)
+  | otherwise = pos : path (movePos pos 1 0) East (int-1)
+path pos West int
+  | col pos == 'a' = path pos East (int)
+  | otherwise = pos : path (movePos pos (1) 0) West (int-1)
+path pos South int
+  | row pos == 1 = path pos North (int)
+  | otherwise = pos : path (movePos pos 0 (1)) South (int-1)
+path pos SouthEast int
+  | col pos == 'f' = path pos SouthWest (int)
+  | row pos == 1 = path pos NorthEast (int)
+  | otherwise = pos : path (movePos pos (1) (-1)) SouthEast (int-1)
+path pos SouthWest int
+  | col pos == 'a' = path pos SouthEast (int)
+  | row pos == 1 = path pos NorthWest (int)
+  | otherwise = pos : path (movePos pos (-1) (-1)) SouthWest (int-1)
+path pos NorthEast int
+  | col pos == 'f' = path pos NorthWest (int)
+  | row pos == 6 = path pos SouthEast (int)
+  | otherwise = pos : path (movePos pos 1 (1)) NorthEast (int-1)
+path pos NorthWest int
+  | col pos == 'a' = path pos NorthEast (int)
+  | row pos == 6 = path pos SouthWest (int)
+  | otherwise = pos : path (movePos pos (-1) (1)) NorthWest (int-1)
